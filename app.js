97d7e75965899fe45a329f45146277beb4e6ac09
Aakash006 <aakashpatel141@gmail.com>
2021-02-20 18:59:45 -0500

    add request for html

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const homesRouter = require('./routes/homes.js');
+const homesRouter = require('./routes/homes');
 const cors = require('cors');
+const path = require('path');
 require('dotenv/config');
 const app = express();
 
@@ -24,7 +25,13 @@ app.use(bodyParser.json());
 app.use('/api', homesRouter); // for all homes requests send request to homes router
 
 if (process.env.NODE_ENV === "production") {
+
+    //set static file with js and css
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    });
 }
 // Listen to server
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));

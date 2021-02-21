6c95abfd9a131a92ec4f83dfec6d7e1978667802
Aakash006 <aakashpatel141@gmail.com>
2021-02-20 23:29:56 -0500

    trying path.join

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(_dirname, '../client', 'build', 'index.html'));
+        res.sendFile(path.join(path.resolve(path.dirname('')), '../client', 'build', 'index.html'));
     });
     
 }
